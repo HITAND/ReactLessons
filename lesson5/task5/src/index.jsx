4b1622aa1d4d5bc05bc85f50c4f934da3b3847de
Andrew <70034920+HITAND@users.noreply.github.com>
2023-05-02 16:44:14 +0300

    Delete lesson5/task5 directory

--- lesson5/task5/src/index.jsx
+++
@@ -1,8 +0,0 @@
-﻿import React from "react"
-import ReactDOM from "react-dom"
-import "./index.scss"
-import Toggler from "./Toggler.jsx"
-
-const rootElem = document.querySelector("#root")
-
-ReactDOM.render(<Toggler />, rootElem)
