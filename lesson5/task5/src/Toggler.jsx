2c48c6b66036bc3c997c038b7c2ca17f470e98f2
HITAND <polishball.pl12@gmail.com>
2023-05-02 16:40:45 +0300

    task5

---
+++ lesson5/task5/src/Toggler.jsx
@@ -0,0 +1,29 @@
+﻿import React from "react"
+import "./index.scss"
+
+class Toggler extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.text = { text1: "Off" }
+  }
+
+  changeText = () => {
+    this.setState({
+      text1:
+        this.text.text1 === "Off"
+          ? (this.text.text1 = "On")
+          : (this.text.text1 = "Off"),
+    })
+  }
+
+  render() {
+    return (
+      <div class="toggler" onClick={this.changeText}>
+        {this.text.text1}
+      </div>
+    )
+  }
+}
+
+export default Toggler
