6b97160835796eef9e1134a78b726767bb66d96f
Andrew <70034920+HITAND@users.noreply.github.com>
2023-05-11 12:12:09 +0300

    Update and rename Message.jsx to Info.jsx

--- lesson6/task3/src/Info.jsx
+++ lesson6/task3/src/Info.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Message = ({text}) => {
+const Info = ({text}) => {
   if(!text) {
     return null;
   }
   return (
-    <div className="message">
+    <div className="info">
       {text}
     </div>
   )
 }
-export default Message;
+export default Info;
