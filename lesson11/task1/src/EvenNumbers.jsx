fa315196bd1e9be7a8cd019d33485012ff37492a
HITAND <polishball.pl12@gmail.com>
2023-06-29 15:34:41 +0300

    lesson11

---
+++ lesson11/task1/src/EvenNumbers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Component } from "react";
+
+class EvenNumbers extends Component {
+  shouldComponentUpdate(props) {
+    return props.number % 2 === 0;
+  }
+  render() {
+    return (
+      <div className="number">
+        <span className="number__title">{this.props.title}</span>
+        <span className="number__value">{this.props.number}</span>
+      </div>
+    );
+  }
+}
+
+export default EvenNumbers;
---
+++ lesson11/task3/src/Expand.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Expand extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  handleExpand = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    let arrow;
+    let children;
+    if (this.state.isOpen === false) {
+      children = null;
+      arrow = <i className="fa-solid fa-arrow-down"></i>;
+    } else {
+      children = this.props.children;
+      arrow = <i className="fa-solid fa-arrow-up"></i>;
+    }
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.handleExpand}>
+            {arrow}
+          </button>
+        </div>
+        <div className="expand__content">{children}</div>
+      </div>
+    );
+  }
+}
+Expand.propTypes = {
+  isOpen: PropTypes.bool,
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string,
+  handleExpand: PropTypes.func,
+};
+
+Expand.defaulProps = {
+  isOpen: false,
+  title: "",
+};
+export default Expand;
